78a47a2a7efbb84f4f2f93fb980c5ca2eb8ac72b
root <louis.officialz@gmail.com>
2024-09-05 02:01:36 +0000

    server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import router from './routes/index.js';
+import router from './routes/index';
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = parseInt(process.env.PORT, 10) || 5000;
 
 app.use(express.json());
 app.use('/', router);
