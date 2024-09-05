1b4da2eebec1b3160cea23a917adf556a99b5ab4
root <louis.officialz@gmail.com>
2024-09-05 01:50:41 +0000

    Utils

--- utils/redis.js
+++ utils/redis.js
@@ -1,10 +1,11 @@
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+// Class to define methods for used redis commands
 class RedisClient {
   constructor() {
     this.client = createClient();
-    this.client.on('error', (err) => {
+    this.client.on('error', (error) => {
       console.log(`Redis client not connected to server: ${error}`);
     });
    }
@@ -17,21 +18,21 @@ class RedisClient {
      return false;
    }
 
-   // get value for given key from redis server
+   // Get value for given key from redis server
   async get(key) {
     const redisGet = promisify(this.client.get).bind(this.client);
     const value = await redisGet(key);
     return value;
   }
 
-  // set key value pair to redis server
+  // Set key value pair to redis server
   async set(key, value, time) {
     const redisSet = promisify(this.client.set).bind(this.client);
     await redisSet(key, value);
     await this.client.expire(key, time);
   }
 
-  // del key vale pair from redis server
+  // Del key vale pair from redis server
   async del(key) {
     const redisDel = promisify(this.client.del).bind(this.client);
     await redisDel(key);
